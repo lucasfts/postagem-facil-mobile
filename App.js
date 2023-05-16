6588382ddac32b55f3da31c7a6eb2afc855d85e3
Lucas Silva <luckkass89@gmail.com>
2023-05-16 20:08:03 -0300

    Adicionando NativeBase ao projeto

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Box, VStack, Center } from "native-base";
+import { Button } from "native-base";
+import Footer from "./components/footer";
+import Form from "./components/form";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1}>
+        <Form />
+      </Center>
+      <Center>
+        <Footer />
+      </Center>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
